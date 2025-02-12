6221426a471095c3603b934dad9b4df348eb6537
amumu <136903859@qq.com>
2025-02-12 16:23:39 +0800

    add a demo of SnowFlake

--- src/completableFuture/Main.java
+++ src/completableFuture/Main.java
@@ -1,11 +1,18 @@
 package completableFuture;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            testFutureTask();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         ExecutorService executor = Executors.newFixedThreadPool(5);
         CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> {
             System.out.println("执行step 1");
@@ -20,5 +27,14 @@ public class Main {
             System.out.println("执行step 3");
             return "step3 result";
         }).thenAccept(result3 -> System.out.println(result3));
+    }
+
+    public static void testFutureTask() throws ExecutionException, InterruptedException {
+        FutureTask task = new FutureTask(() -> {
+            System.out.println("执行异步call方法");
+            return 1;
+        });
+        new Thread(task).start();
+        System.out.println("异步结果:"+task.get());
     }
 }
